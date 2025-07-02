f39f68cf608e9785cf824efe901fdd8f49d34f0e
Intissar Gourainy <I.gourainy@aui.ma>
2025-07-02 15:47:44 +0100

    Update schema_setup.sql

--- sql/schema_setup.sql
+++ sql/schema_setup.sql
@@ -1,1 +1,11 @@
+CREATE TABLE raw_crop_data (
+  Region VARCHAR(50),
+  Crop VARCHAR(50),
+  Soil_Type VARCHAR(50),
+  Nitrogen INT,
+  Phosphorus INT,
+  Potassium INT,
+  Yield_Tons FLOAT,
+  Fertilizer_Type VARCHAR(50)
+);
 
